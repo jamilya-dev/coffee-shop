316580391244689b00a1aa3ad80c544c23488066
jamilya-dev <omg.groups@yandex.ru>
2024-10-03 11:52:24 +0300

    added dinamik import

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,29 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import HeroView from '../views/HeroView.vue';
-import OurCoffeeView from '../views/OurCoffeeView.vue';
-import PleasureView from '../views/PleasureView.vue';
-import ContactsView from '../views/ContactsView.vue';
-import ThankYouView from '../views/ThankYouView.vue';
-import ItemPageView from '../views/ItemPageView.vue';
-
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', component: HeroView },
-  { path: '/our-coffee', component: OurCoffeeView },
-  { path: '/pleasure', component: PleasureView },
-  { path: '/contacts', component: ContactsView },
-  { path: '/thanks', component: ThankYouView },
+  {
+    path: '/',
+    component: () => import('../views/HeroView.vue'),
+  },
+  {
+    path: '/our-coffee',
+    component: () => import('../views/OurCoffeeView.vue'),
+  },
+  {
+    path: '/pleasure',
+    component: () => import('../views/PleasureView.vue'),
+  },
+  {
+    path: '/contacts',
+    component: () => import('../views/ContactsView.vue'),
+  },
+  {
+    path: '/thanks',
+    component: () => import('../views/ThankYouView.vue'),
+  },
 ];
 
 const router = new VueRouter({
